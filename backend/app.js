b8faacb14cbb500b2780c8392f6e6c7da0decbbe
Suryansh Panwar <panwarsuryansh2@gmail.com>
2022-05-27 19:19:13 +0530

    added chat file

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const { chats } = require("./data/data")
 const app = express()
 const port = 8000
 
 app.get('/', (req, res) =>{
      res.send('Hello World!')
+})
+app.get("/api/chat" , (req , res) =>{
+    res.send(chats);
 })
 
 
