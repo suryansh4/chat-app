a5a4fc571602017819d7a7ef6794a6b505d48f2c
Suryansh Panwar <panwarsuryansh2@gmail.com>
2022-05-27 19:42:43 +0530

    configed chat app

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,22 @@
 const express = require('express')
-const { chats } = require("./data/data")
+const chat  = require("./data/data")
+const dotenv = require('dotenv');
 const app = express()
-const port = 8000
+
+dotenv.config();
+const port = process.env.PORT || 8000
 
 app.get('/', (req, res) =>{
      res.send('Hello World!')
 })
 app.get("/api/chat" , (req , res) =>{
-    res.send(chats);
+    res.send(chat);
 })
 
+app.get("/api/chat/:id" , (req , res) =>{
+    console.log(req.params.id)
+  const singlechat = chat.find((c)=>c._id ===req.params.id)
+  res.send(singlechat);
+})
 
 app.listen(port, console.log(`Example app listening on port ${port}!`))
