aa82c85eb15d456fade109114e0a7548775117a2
Suryansh Panwar <panwarsuryansh2@gmail.com>
2022-06-04 12:57:44 +0530

    designing home page

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,8 +1,55 @@
 import React from 'react'
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <Container maxW='xl' centerContent>
+    <Box
+     d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px">
+       <Text fontSize="4xl" fontFamily="Work sans">
+          Let's Chat
+        </Text>
+    </Box>
+    <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+    </Box>
+    
+
+    </Container>
   )
 }
 
